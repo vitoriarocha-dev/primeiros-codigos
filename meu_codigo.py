13ab359cf1cc6bdb61a5f9b68390cb921a49bd03
Maria00725 <maryvoliveirarocha@gmail.com>
2026-08-26 20:49:51 -0300

    Create meu_codigo.py

---
+++ meu_codigo.py
@@ -0,0 +1,8 @@
+# Programa em Python
+
+nome = input("Qual é o seu nome?")
+idade = int(input("Quantos anos você tem?"))
+
+print("\n--- Resultado ---")
+print(f"Olá, Maria!")
+print(f"Voce tem 19 anos e em 4 anos se formará na faculdade com 23 anos.")
